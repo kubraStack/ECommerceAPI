216930d9bbe204c34cf512d6c8e677cac115e988
kubrastack <kmrt3014@gmail.com>
2024-10-02 15:19:32 +0300

    added query

--- Application/Features/Order/Queries/GetAllOrders/GetAllOrdersQuery.cs
+++ Application/Features/Order/Queries/GetAllOrders/GetAllOrdersQuery.cs
@@ -3,9 +3,11 @@ using Application.Features.OrderDetails.DTOS;
 using Application.Repositories;
 using Domain.Entities;
 using MediatR;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,29 +18,54 @@ namespace Application.Features.Order.Queries.GetAllOrders
         public class GetAllOrdersQueryHandler : IRequestHandler<GetAllOrdersQuery, GetAllOrdersQueryResponse>
         {
             private readonly IOrderRepository _orderRepository;
+            private readonly IHttpContextAccessor _httpContextAccessor;
 
-            public GetAllOrdersQueryHandler(IOrderRepository orderRepository)
+
+            public GetAllOrdersQueryHandler(IOrderRepository orderRepository, IHttpContextAccessor httpContextAccessor)
             {
                 _orderRepository = orderRepository;
+                _httpContextAccessor = httpContextAccessor;
             }
 
             public async Task<GetAllOrdersQueryResponse> Handle(GetAllOrdersQuery request, CancellationToken cancellationToken)
             {
-                var orders = await _orderRepository.GetListAsync();
+                // Kullanıcı rolünü almak için HttpContextAccessor kullanıyoruz
+                var userRole = _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.Role)?.Value;
+                var userId = _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
 
-                //OrderDto'ya dönüştürme
-                var orderDtos = orders.Select(order => new OrderDto
+                // Eğer kullanıcı admin ise, tüm siparişleri al
+                if (userRole == "Admin")
+                {
+                    var orders = await _orderRepository.GetListAsync();
+
+                    var orderDtos = orders.Select(order => new OrderDto
+                    {
+                        OrderId = order.Id,
+                        OrderDate = (DateTime)order.OrderDate,
+                        TotalAmount = order.TotalAmount,
+                        OrderStatusId = order.OrderStatusId,
+                    }).ToList();
+
+                    return new GetAllOrdersQueryResponse
+                    {
+                        Orders = orderDtos
+                    };
+                }
+
+                // Eğer kullanıcı customer ise, sadece kendi siparişlerini al
+                var customerOrders = await _orderRepository.GetListAsync(order => order.CustomerId.ToString() == userId);
+
+                var customerOrderDtos = customerOrders.Select(order => new OrderDto
                 {
                     OrderId = order.Id,
                     OrderDate = (DateTime)order.OrderDate,
                     TotalAmount = order.TotalAmount,
-                    OrderStatusId = order.OrderStatusId, // Eğer OrderDto'da statusId eklediyseniz
-                   
+                    OrderStatusId = order.OrderStatusId,
                 }).ToList();
 
                 return new GetAllOrdersQueryResponse
                 {
-                    Orders = orderDtos // OrderDto listesini yanıt nesnesine ekleyin
+                    Orders = customerOrderDtos
                 };
             }
         }
